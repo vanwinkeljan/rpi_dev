886b61ee8509c03ec5608bc90ef9e73bff39d21e
Jan Van Winkel <vanwinkeljan@gmail.com>
2013-01-19 12:44:20 +0100

    Added small test program to test cross compiler

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+int main(int argc, char **argv){
+  float f=1.0;
+  std::cout << "Float f = " << f << std::endl;
+  f+=0.5;
+  std::cout << "Float f+0.5 = " << f << std::endl;
+  return 0;
+}
